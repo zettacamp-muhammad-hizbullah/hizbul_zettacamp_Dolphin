a03636fc18a93619e4d470fdabe796a94e976da4
Muhammad Hizbullah <muhammad.hizbullah@zettacamp.pro>
2023-09-13 09:02:12 +0700

    expressjs day2

--- expressjs/purchasing-book/routes/routes.js
+++ expressjs/purchasing-book/routes/routes.js
@@ -1,4 +1,3 @@
-const { asyncAwaitFunction, asyncNotAwaitFunction } = require('../controllers/async-function.controller');
 const { purchaseBook } = require('../controllers/book.controller');
 const adminMiddleware = require('../middlewares/admin.middleware');
 const { bookPurchaseValidator } = require('../validators/book.validator');
@@ -6,8 +5,6 @@ const { bookPurchaseValidator } = require('../validators/book.validator');
 module.exports = (app) => {
   app.use(adminMiddleware);
   app.post('/books', bookPurchaseValidator, purchaseBook);
-  app.get('/async-await', asyncAwaitFunction);
-  app.get('/async-not-await', asyncNotAwaitFunction);
   app.all('*', function (req, res) {
     res.status(404).json({
       success: false,
