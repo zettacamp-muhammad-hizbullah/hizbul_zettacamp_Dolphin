c9c63e0e9c9982ff76ccb9a8af1e8adad758b388
Muhammad Hizbullah <muhammad.hizbullah@zettacamp.pro>
2023-10-03 15:46:37 +0700

    update query dataloader

--- expressjs/song-playlist/graphql/loaders/playlist.loader.js
+++ expressjs/song-playlist/graphql/loaders/playlist.loader.js
@@ -1,10 +1,16 @@
 const DataLoader = require('dataloader');
 const playlistService = require('../../services/playlist.service');
+const Model = require('../models/index.model');
 
 exports.playlistLoader = new DataLoader(
   async (keys) => {
     // console.log('keys', keys);
-    const result = await playlistService.retriveAllPlaylist();
+    // const result = await playlistService.retriveAllPlaylist();
+    const result = await Model.playlist.find({
+      _id: {
+        $in: keys,
+      },
+    });
     const dataMap = new Map();
     result.forEach((item) => dataMap.set(String(item._id), item));
     // console.log('dataMap', dataMap);
--- expressjs/song-playlist/graphql/loaders/song.loader.js
+++ expressjs/song-playlist/graphql/loaders/song.loader.js
@@ -1,10 +1,16 @@
 const DataLoader = require('dataloader');
 const songService = require('../../services/song.service');
+const Model = require('../models/index.model');
 
 exports.songLoader = new DataLoader(
   async (keys) => {
     // console.log('keys', keys);
-    const result = await songService.retriveAllSong();
+    // const result = await songService.retriveAllSong();
+    const result = await Model.song.find({
+      _id: {
+        $in: keys,
+      },
+    });
     const dataMap = new Map();
     result.forEach((item) => dataMap.set(String(item._id), item));
     return keys.map((key) => dataMap.get(String(key)));
